81e9d3be441c6ca8101d355958b22e7b8d7192ff
Your Name <you@example.com>
2021-09-03 00:02:16 +0600

    Update Backend and Completed Team Model

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
-
+from .models import Team
 # Create your views here.
 def home(request):
-    return render(request, 'pages/home.html')
+    #fetch data
+    teams = Team.objects.all()
+    data = {
+          'teams' : teams,
+    }
+    return render(request, 'pages/home.html', data)
 
 def about(request):
-    return render(request,'pages/about.html')
+    #fetch data
+    teams = Team.objects.all()
+    data = {
+         'teams' : teams,
+    }
+    return render(request,'pages/about.html',data)
 
 def services(request):
     return render(request, 'pages/services.html')
