73a6024c09667021106a0d3dd51cd469ec45a968
Fahadul Islam <fahadulislam.user@outlook.com>
2021-09-07 11:19:09 +0600

    many thing done

--- carzone/settings.py
+++ carzone/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'cars.apps.CarsConfig',
     'pages.apps.PagesConfig',
     'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -138,5 +139,14 @@ MEDIA_URL = '/media/'
 #messages
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
-    messages.ERROR: 'danger',    
+    messages.ERROR: 'danger',
 }
+
+
+
+# Email sending
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'fahadulislam112@gmail.com'
+EMAIL_HOST_PASSWORD = '12345'
+EMAIL_USE_TLS = True
